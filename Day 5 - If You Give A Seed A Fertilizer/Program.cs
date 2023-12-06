dc37a4bfd05eb0253ba1adb3ee7e65ed4b2699cb
Fabian Krohn <f.krohn@gfe-burghausen.de>
2023-12-06 08:34:59 +0100

    Splitting up input for Day 5

---
+++ Day 5 - If You Give A Seed A Fertilizer/Program.cs
@@ -0,0 +1,16 @@
+﻿String[] input = File.ReadAllLines("C:\\Users\\kro\\Documents\\Projekte\\AdventOfCode2023\\Day 5 - If You Give A Seed A Fertilizer\\testInput.txt");
+
+List<List<String>> sections = new List<List<String>>();
+
+List<String> section = new List<string>();
+
+foreach(String line in input){
+    if(line == ""){
+        sections.Add(section);
+        section = new List<string>();
+        continue;
+    }
+    section.Add(line);
+}
+
+Console.WriteLine("HI!");
