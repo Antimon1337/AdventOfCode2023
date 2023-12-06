b8aa88ea67e706def0d069e5d7430f308bab1d25
Fabian - Altes MacBook <antimon1337@outlook.de>
2023-12-06 20:04:13 +0100

    Added solution for Part 1

--- Day 6 - Wait For It/Program.cs
+++ Day 6 - Wait For It/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+String[] input = File.ReadAllLines("/Users/fabiankrohn/Projekte/AdventOfCode2023/Day 6 - Wait For It/input.txt");
+
+int produkt = 1;
+
+List<int> gameTimes = new List<int>();
+List<int> records = new List<int>();
+
+foreach(String time in input[0].Split(':')[1].Split(' ')){
+    if(time.Trim() == "") continue;
+    gameTimes.Add(int.Parse(time.Trim()));
+}
+
+foreach(String record in input[1].Split(':')[1].Split(' ')){
+    if(record.Trim() == "") continue;
+    records.Add(int.Parse(record.Trim()));
+}
+
+for(int i = 0; i < gameTimes.Count; i++){
+    Console.WriteLine("Game " + (i+1) + ": ");
+    int wins = 0;
+    for(int timeHeld = 0; timeHeld < gameTimes[i]; timeHeld++){
+        int lengthDriven = timeHeld * (gameTimes[i]-timeHeld);
+        if(lengthDriven > records[i]){
+            Console.WriteLine("Du gewinnst!");
+            Console.WriteLine("timeHeld: " + timeHeld);
+            Console.WriteLine("lengthDriven: " + lengthDriven);
+            Console.WriteLine();
+            wins++;
+        }
+    }
+    produkt *= wins;
+}
+
+Console.WriteLine(produkt);
