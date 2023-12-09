16efabbdf6250c6d743643aace8a5673c93e33d0
Fabian - Altes MacBook <antimon1337@outlook.de>
2023-12-09 10:53:14 +0100

    Gave up

--- Day 8 - Hauted Wasteland/obj/Debug/net8.0/Day 8 - Hauted Wasteland.AssemblyInfo.cs
+++ Day 8 - Hauted Wasteland/obj/Debug/net8.0/Day 8 - Hauted Wasteland.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day 8 - Hauted Wasteland")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b95d56be9bed05541090c90d7fdf45c5c07e65f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day 8 - Hauted Wasteland")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day 8 - Hauted Wasteland")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
