6e441413d207bf4716bf40ac790174043cf3f348
Fabian - Altes MacBook <antimon1337@outlook.de>
2023-12-13 19:54:21 +0100

    kb mehr

---
+++ Day 9 - Mirage Maintenance/Program.cs
@@ -0,0 +1,28 @@
+﻿String[] input = File.ReadAllLines("/Users/fabiankrohn/Projekte/AdventOfCode2023/Day 9 - Mirage Maintenance/testInput.txt");
+
+for(int i = 0; i < input.Length; i++){
+    List<long> sequence = new List<long>();
+    foreach(String s in input[i].Split(' ')){
+        sequence.Add(int.Parse(s));
+    }
+
+    List<long> differenzen = sequence;
+    while(!differenzenOK(differenzen)){
+        for(int j = 0; j < differenzen.Count-1; j++){
+            differenzen.Add(differenzen[j+1] - differenzen[j]);
+            Console.WriteLine(differenzen[j+1] - differenzen[j]);
+            
+        }
+    }
+    Console.WriteLine();   
+}
+
+bool differenzenOK(List<long> diff){
+    bool notok = true;
+    foreach(long d in diff){
+        if(d != 0){
+            notok = false;
+        }
+    }
+    return notok;
+}
